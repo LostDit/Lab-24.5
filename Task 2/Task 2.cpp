d469f2fa1372722946f673d9d1969571c7a6db75
Maksim <maksim.antonov2012@yandex.ru>
2025-03-23 08:21:24 +0300

    App

---
+++ Task 2/Task 2.cpp
@@ -0,0 +1,177 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <limits>
+#include <iomanip>
+
+#define NOMINMAX
+#include<windows.h>
+
+struct FriendBirthday {
+
+	std::string name;
+	int month;
+	int day;
+
+};
+
+
+/*Функция для парсинга даты в формате "год/месяц/день"
+Здесь год сохраняется, но в дальнейшем не используется - нас интересуют только месяц и день*/
+bool parseDate(const std::string& dateStr, int& year, int& month, int& day) {
+
+	std::istringstream iss(dateStr);
+	char sep;
+	if (!(iss >> year >> sep >> month >> sep >> day)) {
+		return false;
+	}
+	return true;
+}
+
+
+//Функция для ввода иформации о днях рождения друзей
+std::vector<FriendBirthday> readBirthdays() {
+	std::vector<FriendBirthday> friends;
+	std::string inputName;
+	std::string inputDate;
+
+	while (true)	{
+		std::cout << "Введите данные о дне рождения друзей.\n";
+		std::cout << "Сначала имя, затем дата рождения в формате год(2001)/месяц(01-12)/день(01-30)\n";
+		std::cout << "Для завершения ввода введите \"end\" вместо имени.\n";
+		
+		std::cout << "Имя: ";
+		std::cin >> inputName;
+		
+		if (inputName == "end")	break;
+	
+		std::cout << "Дата(год/месяц/день): ";
+		std::cin >> inputDate;
+		
+		int year, month, day;
+		
+		if (!parseDate(inputDate, year, month, day)) {
+			std::cout << "Неверный формат даты. Попробуйте ещё раз.\n";
+			system("pause");
+			system("cls");
+			continue;
+		}
+
+		FriendBirthday fb;
+		fb.name = inputName;
+		fb.month = month;
+		fb.day = day;
+		friends.push_back(fb);
+		
+		system("pause");
+		system("cls");
+	}
+
+	return friends;
+}
+
+
+/*Функция котороя отвечает за формирование даты дня рождения для друга в текущем году,
+так же возвращает time_t текущего дня (если точнее, то в полночь)*/
+std::time_t makeBirthdayTime(const FriendBirthday& fb, const std::tm& currentTm) {
+	
+	std::tm birthdayTm = currentTm;	    //Копирую текущую дату
+	birthdayTm.tm_mon = fb.month - 1;	//Корректирую месяц (от 0 до 11)
+	birthdayTm.tm_mday = fb.day;
+	birthdayTm.tm_hour = 0;
+	birthdayTm.tm_min = 0;
+	birthdayTm.tm_sec = 0;
+
+	//mktime заполнит другие поля структуры tm (tm_wday и tm_yday)
+	return std::mktime(&birthdayTm);
+}
+
+
+/*Функция для вычисления ближайщих предстоящих дней рождения, 
+если день рождения уже прошёл в текущем году, запись пропускается, 
+если несколько друзей имеют одинаковый ближайщий день, то они все добавляются*/
+std::vector<FriendBirthday> computeUpcomingBirthdays(const std::vector<FriendBirthday>& friends) {
+	std::vector <FriendBirthday> result;
+	
+	//Получаем текущий момент времени и структуру tm
+	std::time_t now = std::time(nullptr);
+	std::tm currentTm = *std::localtime(&now);
+
+	double minDiff = std::numeric_limits<double>::max();
+
+	//Определяю ближайщую разницу во времени в секундах.
+	for (const auto &fb:friends)	{
+		std::time_t btime = makeBirthdayTime(fb, currentTm);
+		//Вычисляем текущую разницу между днём рождения и текущим временем
+		double diff = std::difftime(btime, now);
+
+		//Если день рождения уже прошёл,пропускаем запись.
+		if (diff < 0)
+			continue;
+		
+		//Если нашли более ранний день рождения, обновляем минимальную разницу и очищаем результат
+		if (diff< minDiff) {
+			minDiff = diff;
+			result.clear();
+			result.push_back(fb);
+		}else if (diff==minDiff)	{		//Если разница равна текущей минимальной разнице, добавляем друга в список
+			result.push_back(fb);
+		}
+	}
+
+	return result;
+}
+
+
+//Функция для форматированного вывода результатов
+void printUpcomingBirthdays(const std::vector<FriendBirthday>& upcoming){
+	if (upcoming.empty()) {
+		std::cout << "Нет предстоящих дней рождения в этом году.\n";
+		return;
+	}
+
+	//Получаю текущий момент времени и структуру tm
+	std::time_t now = std::time(nullptr);
+	std::tm currentTm = *std::localtime(&now);
+
+	for (const auto& fb : upcoming) {
+		
+		//Форматируем дату: месяц/день с ведущими нулями
+		std::ostringstream oss;
+		oss << std::setfill('0') << std::setw(2) << fb.month << "/" << std::setfill('0') << std::setw(2) << fb.day;
+	
+		//Вычисление разницы чтобы узнать есть ли "сегодня" др
+		std::time_t btime = makeBirthdayTime(fb, currentTm);
+		double diff = std::difftime(btime, now);
+
+		if (diff <= 0.0) {
+			std::cout << "Сегодня день рождения у: " << fb.name << " (" << oss.str() << ")\n";
+		}else {
+			std::cout << "Ближайщий день рождения у: " << fb.name << " (" << oss.str() << ")\n";
+		}
+	}
+}
+
+int main()	{
+	setlocale(LC_ALL, "");
+	SetConsoleCP(866);
+	SetConsoleOutputCP(866);
+
+	std::vector<FriendBirthday> friends = readBirthdays();
+
+	if (friends.empty()) {
+		std::cout << "Нет введённых данных. Завершение.\n";
+		return 0;
+	}
+
+	//Вычисление ближайщих дней рождения
+	std::vector<FriendBirthday> upcoming = computeUpcomingBirthdays(friends);
+
+	//Вывод полученных результатов
+	printUpcomingBirthdays(upcoming);
+
+	return 0;
+}
---
+++ Task 3/Task 3.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <ctime>
+#include <thread>
+#include <string>
+#include <windows.h>
+
+/*Функция для разбора строки времени в формате MM : SS и расчёта общего количества секунд.
+При неверном формате времени возвращает отрицательное значение.*/
+int parseTime() {
+	std::cout << "Введите время в формате MM:SS: ";
+	std::string input;
+	std::getline(std::cin, input);
+
+	std::tm t = {};
+	std::istringstream iss(input);
+	if (!(iss >> std::get_time(&t, "%M:%S"))) {
+		std::cerr << "Неверный формат ввода!\n";
+		return -1;
+	}
+
+	return t.tm_min * 60 + t.tm_sec;
+}
+
+//Функция запуска обратного отсчёта таймера, в качестве параметра передаётся общее время в секундах
+void runTimer(int totalSeconds) {
+    auto startTime = std::chrono::steady_clock::now();
+    auto endTime = startTime + std::chrono::seconds(totalSeconds);
+
+    while (true) {
+        auto currentTime = std::chrono::steady_clock::now();
+        auto remainingDuration = std::chrono::duration_cast<std::chrono::seconds>(endTime - currentTime);
+        int remainingSeconds = remainingDuration.count();
+
+        if (remainingSeconds < 0) {
+            break;
+        }
+
+        // Преобразование оставшихся секунд в минуты и секунды.
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+
+        // Вывод оставшегося времени с форматированием и перезаписью всей строки.
+        std::cout << "\r"<< std::setw(2) << std::setfill('0') << minutes << ":"<< std::setw(2) << std::setfill('0') << seconds << std::flush;
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    std::cout << "\nDING! DING! DING!\a" << std::endl;
+}
+
+
+int main()	{
+	setlocale(LC_ALL, "");
+	SetConsoleCP(866);
+	SetConsoleOutputCP(866);
+	int totalSeconds = parseTime();
+	if (totalSeconds < 0) {
+		return 1;
+	}
+
+	runTimer(totalSeconds);
+
+	return 0;
+}
